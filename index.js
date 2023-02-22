840ecf0e1a1df3800bae27136912235ffdc0cdc2
Narayana <nanigudimetla990@gmail.com>
2023-02-22 22:55:14 +0530

    first

---
+++ index.js
@@ -0,0 +1,14 @@
+// const options = {
+//   method: "POST",
+//   headers: {
+//     "content-type": "application/json",
+//     "X-RapidAPI-Key": "7e4d46dc85msh4f03f1a4e929061p191d0cjsn3afedf048961",
+//     "X-RapidAPI-Host": "rapidprod-sendgrid-v1.p.rapidapi.com",
+//   },
+//   body: '{"personalizations":[{"to":[{"email":"nanigudimetla990@gmail.com"}],"subject":"Hello, World!"}],"from":{"email":"nanigudimetla990@gmail.com"},"content":[{"type":"text/plain","value":"Hello, World!"}]}',
+// };
+
+// fetch("https://rapidprod-sendgrid-v1.p.rapidapi.com/mail/send", options)
+//   .then((response) => response.json())
+//   .then((response) => console.log(response))
+//   .catch((err) => console.error(err));
